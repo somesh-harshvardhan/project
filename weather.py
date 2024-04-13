564774948b8dfd1107c6499ad58f475b97abda4d
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2024-04-14 02:08:42 +0530

    first python flask app

---
+++ weather.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city="Gaya"):
+    request_url=f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}"
+
+    weather_data=requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input("\nPlease enter a city name: ")
+
+    #Check for emoty strings or string with only spaces
+
+    if not bool(city.strip()):
+        city ="Gaya"
+
+    weather_data=get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
